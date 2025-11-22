bc1957874b892763c5fd5568b6528ba16e565beb
aldinpramudya <aldinarielpramudya@gmail.com>
2025-11-23 01:44:41 +0700

    feat(styling) : adding tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,14 @@
+<script setup>
+useHead({
+  titleTemplate : "Web Engineer App"
+})
+</script>
+
 <template>
   <div>
+    <!-- Nuxt Component Default Testing -->
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- Nuxt Component Default End -->
+     <NuxtPage />
   </div>
 </template>
