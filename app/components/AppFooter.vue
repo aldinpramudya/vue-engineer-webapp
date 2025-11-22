1d6e4a43525cc314089188fdefc02e3dcaa2af00
aldinpramudya <aldinarielpramudya@gmail.com>
2025-11-23 02:59:44 +0700

    feat(Footer) : Footer Component

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-        <p>this is footer</p>
-    </div>
+    <footer class="bg-gray-200 text-gray-300">
+        <div class="max-w-7xl mx-auto">
+            <div class="py-5 text-center text-sm text-gray-500">
+                <p>&copy; {{ currentYear }} TransactionApp. Code by Riel.</p>
+            </div>
+        </div>
+    </footer>
 </template>
 
 <script setup>
-
+const currentYear = new Date().getFullYear()
 </script>
-
-<style lang="scss" scoped>
-
-</style>
